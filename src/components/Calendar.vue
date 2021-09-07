d8ec49892228c2f762d01c90334650ba8dd289d4
parijat-technicise <parijatrudraroy75@gmail.com>
2021-09-07 13:48:11 +0530

    Show obejct in calendar component in label

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,76 +1,104 @@
 <template>
-  <v-row>
+  <v-row class="fill-height">
     <v-col>
-      <v-sheet height="400">
+      <v-sheet height="64">
+        <v-toolbar flat>
+          <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
+            Today
+          </v-btn>
+          {{ event }}
+          <v-btn fab text small color="grey darken-2" @click="prev">
+            <v-icon small> mdi-chevron-left </v-icon>
+          </v-btn>
+          <v-btn fab text small color="grey darken-2" @click="next">
+            <v-icon small> mdi-chevron-right </v-icon>
+          </v-btn>
+          <v-toolbar-title v-if="$refs.calendar">
+            {{ event.name }}
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+      </v-sheet>
+      <v-sheet height="600">
         <v-calendar
           ref="calendar"
-          :now="today"
-          :value="today"
-          :events="event"
+          v-model="focus"
           color="primary"
-          type="week"
+          type="category"
+          :events="events"
+          @change="fetchEvents"
         ></v-calendar>
       </v-sheet>
     </v-col>
   </v-row>
 </template>
-
-<script lang='ts'>
+<script lang="ts">
+import Vue from "vue";
 import { EventObject } from "../Model";
-export default {
+export default Vue.extend({
+  name: "Calendar",
+  props: { event: [] as EventObject[] },
   data: () => ({
-    // props: { updateEventData: [] },
-    date: new Date(),
-    today: this.date.formatDate(
-      new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
-        .toISOString()
-        .substr(0, 10)
-    ),
-    // events: [
-    //   {
-    //     name: "Weekly Meeting",
-    //     start: "2021-09-01 09:00",
-    //     end: "2021-09-01 10:00",
-    //   },
-    //   {
-    //     name: `Thomas' Birthday`,
-    //     start: "2019-01-10",
-    //   },
-    //   {
-    //     name: "Mash Potatoes",
-    //     start: "2019-01-09 12:30",
-    //     end: "2019-01-09 15:30",
-    //   },
-    // ],
-    props: { event: [] as EventObject[] },
+    focus: "",
+    events: [],
+    colors: [
+      "blue",
+      "indigo",
+      "deep-purple",
+      "cyan",
+      "green",
+      "orange",
+      "grey darken-1",
+    ],
+    names: [this.event.name],
   }),
   mounted() {
-    this.$refs.calendar.scrollToTime("08:00");
+    this.$refs.calendar.checkChange();
+    console.log("CalendarData", this.objects);
   },
-};
+  methods: {
+    getEventColor(event) {
+      return event.color;
+    },
+    setToday() {
+      this.focus = "";
+    },
+    prev() {
+      this.$refs.calendar.prev();
+    },
+    next() {
+      this.$refs.calendar.next();
+    },
+    fetchEvents({ start, end }) {
+      const events = [];
+
+      // const min = new Date(`${start.date}T00:00:00`);
+      // const max = new Date(`${end.date}T23:59:59`);
+      // const days = (max.getTime() - min.getTime()) / 86400000;
+      // const eventCount = this.rnd(days, days + 20);
+
+      // for (let i = 0; i < eventCount; i++) {
+      //   const allDay = this.rnd(0, 3) === 0;
+      //   const firstTimestamp = this.rnd(min.getTime(), max.getTime());
+      //   const first = new Date(firstTimestamp - (firstTimestamp % 900000));
+      //   const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
+      //   const second = new Date(first.getTime() + secondTimestamp);
+
+      //   events.push({
+      //     name: this.names[this.rnd(0, this.names.length - 1)],
+      //     start: first,
+      //     end: second,
+      //     color: this.colors[this.rnd(0, this.colors.length - 1)],
+      //     timed: !allDay,
+      //     category: this.categories[this.rnd(0, this.categories.length - 1)],
+      //   });
+      // }
+
+      this.events = this.event;
+    },
+    rnd(a, b) {
+      return Math.floor((b - a + 1) * Math.random()) + a;
+    },
+  },
+});
 </script>
-
-<style scoped>
-.my-event {
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  border-radius: 2px;
-  background-color: #1867c0;
-  color: #ffffff;
-  border: 1px solid #1867c0;
-  font-size: 12px;
-  padding: 3px;
-  cursor: pointer;
-  margin-bottom: 1px;
-  left: 4px;
-  margin-right: 8px;
-  position: relative;
-}
-
-.my-event.with-time {
-  position: absolute;
-  right: 4px;
-  margin-right: 0px;
-}
-</style>
--- src/components/Output.vue
+++ src/components/Output.vue
@@ -5,28 +5,29 @@
         <p>
           Events: <strong>{{ objects.length }}</strong>
         </p>
-        <label>{{objects}}</label>
+        <label>{{ objects }}</label>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
+import Vue from "vue";
 import { EventObject } from "../Model";
 
-export default {
+export default Vue.extend({
   name: "Output",
+  props: { objects: [] as EventObject[] },
   data: function () {
     return {
       formatedObject: "",
     };
   },
-  props: { objects: [] as EventObject[] },
   
   watch: {
     convertObject(objects:any) {
       let formatedObject = objects.tostring();
     },
   },
-};
+});
 </script>
